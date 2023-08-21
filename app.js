9b17a0249fe7c48c178c353fbbcef3450ed10bb1
Oluwaseun <oluwaseunus@gmail.com>
2023-08-21 15:52:09 +0100

    pre express assignment

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes.requestHandler);
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware!");
+  res.send(`
+    <h1>
+      This is the 'Add Products' page
+    </h1>
+  `);
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send(`
+    <h1>
+      Hello from Express.js!
+    </h1>
+  `);
+});
+
+app.listen(3000);
